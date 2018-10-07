22791d65d304d612c20e6168f0d2704b4ed8fb34
Артем Хуршудов <khurshudov@MacBook-Pro-Artem.local>
2018-10-07 10:18:16 +0300

    zoom changes

--- dont_check/src/index.js
+++ dont_check/src/index.js
@@ -24,30 +24,59 @@ function max(a,b) {
 let main_image_left;
 let min_left_progress_bar_window;
 
+
 main_image.style.webkitFilter = 'brightness(.5)';
 // const brightnessBefore = main_image.style.webkitFilter.split('(')[1].split(')')[0];
 // $(".some_text").text(brightnessBefore);
 
-function prepositioning(scaleX) {
+function prepositioning(scaleX, left = null) {
     main_image_left =  (scaleX - 1) / 2 * main_image.width;
+
+    const leftBefore = left !== null ? max(- main_image.getBoundingClientRect().width + img_wrapper.getBoundingClientRect().width +  main_image_left,
+        min(main_image.style.transform.split(',')[4], main_image_left))
+        : main_image_left;
+    // main_image_left = leftBefore;
+
+    // main_image_left =  (scaleX - 1) / 2 * main_image.width;
+    // $(".some_text").text(leftBefore);
+
+
     main_image.style.transform = 'matrix(' +
         scaleX +
         ', 0, 0, 1, ' +
-        main_image_left +
+        leftBefore +
         ', 0)';
 
     const kf = img_wrapper.getBoundingClientRect().width / main_image.getBoundingClientRect().width;
 
     progress_bar.style.width = (img_wrapper.getBoundingClientRect().width / 2.0) + 'px';
 
-    progress_bar_window.style.width = min(progress_bar.getBoundingClientRect().width, (kf * progress_bar.getBoundingClientRect().width)) + 'px';
-    progress_bar_window.style.left = -1 / 4 * progress_bar.getBoundingClientRect().width +
-        1 / 2 * progress_bar_window.getBoundingClientRect().width +
-        progress_bar.getBoundingClientRect().left -
-        8
-        + 'px';
+    let progress_bar_window_before_style_width = progress_bar_window.style.width;
 
-    min_left_progress_bar_window = parseFloat(progress_bar_window.style.left.substring(0, progress_bar_window.style.left.length -2));
+    progress_bar_window.style.width = min(progress_bar.getBoundingClientRect().width, (kf * progress_bar.getBoundingClientRect().width)) + 'px';
+
+
+
+    if(left === null){
+        progress_bar_window.style.left = -1 / 4 * progress_bar.getBoundingClientRect().width +
+            1 / 2 * progress_bar_window.getBoundingClientRect().width +
+            progress_bar.getBoundingClientRect().left -
+            8
+            + 'px';
+
+        min_left_progress_bar_window = parseFloat(progress_bar_window.style.left.substring(0, progress_bar_window.style.left.length -2));
+
+
+    } else {
+        if(progress_bar_window_before_style_width > progress_bar_window.style.width){
+            progress_bar_window.style.left = parseFloat(progress_bar_window.style.left.split('p')[0]) +
+                ( + ( + parseFloat(progress_bar_window_before_style_width.split('p')[0]) - parseFloat(progress_bar_window.style.width.split('p')[0])) / 2)
+                + 'px';
+        }
+
+    }
+
+
 
     $(".zoom").text('zoom 1:' + scaleX.toFixed(2));
 }
@@ -57,7 +86,6 @@ function prepositioning(scaleX) {
     function eventsListeners() {
 
         let previousAngle = 0;
-
 
         let beforeX = 0.0;
 
@@ -96,6 +124,9 @@ function prepositioning(scaleX) {
 
                 if(main_image.getBoundingClientRect().width > img_wrapper.getBoundingClientRect().width){
                     if(beforeX > e.clientX){
+                        // $(".some_text").text(- main_image.getBoundingClientRect().width + img_wrapper.getBoundingClientRect().width +  main_image_left + ' ' +
+                        //     (parseFloat(main_image_transform[4]) - speed) + ' ');
+
                         main_image.style.transform = main_image_transform[0] + ', ' +
                             main_image_transform[1] + ', ' +
                             main_image_transform[2] + ', ' +
@@ -108,6 +139,9 @@ function prepositioning(scaleX) {
                             progress_bar.getBoundingClientRect().width - progress_bar_window.getBoundingClientRect().width + min_left_progress_bar_window) + 'px';
 
                     } else if (beforeX < e.clientX){
+                        // $(".some_text").text(main_image_left + ' ' +
+                        //     (parseFloat(main_image_transform[4]) + speed) + ' ');
+
                         main_image.style.transform = main_image_transform[0] + ', ' +
                             main_image_transform[1] + ', ' +
                             main_image_transform[2] + ', ' +
@@ -160,27 +194,31 @@ function prepositioning(scaleX) {
 
                     const scaleXnew = max(1, parseFloat(main_image_array[0].split('(')[1]) - 0.01);
                     if(scaleXnew !== parseFloat(main_image_array[0].split('(')[1]))
-                        prepositioning(scaleXnew);
+                        prepositioning(scaleXnew, 1);
 
                 } else {
                     const main_image_array =  main_image.style.transform.split(',');
 
                     const scaleXnew = min(4, parseFloat(main_image_array[0].split('(')[1]) + 0.01);
                     if(scaleXnew !== parseFloat(main_image_array[0].split('(')[1]))
-                        prepositioning(scaleXnew);
+                        prepositioning(scaleXnew, 1);
 
                 }
+
+                //brightness 2
 
 
                 const angleBefore = Math.atan2(y1-y2, x1-x2) / Math.PI * 180;
                 let angleAfter = Math.atan2(y1Moved-y2Moved, x1Moved-x2Moved) / Math.PI * 180 - angleBefore;
                 if(angleAfter < 0) angleAfter = 360 + angleAfter;
-                // $(".some_text").text(angleAfter);
 
                 const brightnessBefore = parseFloat(main_image.style.webkitFilter.split('(')[1].split(')')[0]);
                 //$(".some_text").text(brightnessBefore);
 
-                if(previousAngle < angleAfter){
+
+                if(previousAngle - angleAfter < - .5){
+                    // $(".some_text").text(previousAngle + ' ' + angleAfter);
+
                     // $(".some_text").text(previousAngle + ' ' + angleAfter);
                     main_image.style.webkitFilter = 'brightness(' +
                         min(1, brightnessBefore + 1 / 180)+
@@ -188,7 +226,7 @@ function prepositioning(scaleX) {
                     previousAngle = angleAfter;
 
                     $(".brightness").text("brightness: " + (min(1, brightnessBefore + 1 / 180) * 100).toFixed(0) + "%")
-                } else if (previousAngle > angleAfter) {
+                } else if (previousAngle - angleAfter > .5) {
                     // $(".some_text").text(previousAngle + ' ' + angleAfter);
                     main_image.style.webkitFilter = 'brightness(' +
                         min(1, brightnessBefore - 1 / 180)+
@@ -198,8 +236,6 @@ function prepositioning(scaleX) {
                     $(".brightness").text("brightness: " + (max(0, brightnessBefore - 1 / 180) * 100).toFixed(0) + "%")
                 }
 
-
-                //brightness 2
             }
         }
 
